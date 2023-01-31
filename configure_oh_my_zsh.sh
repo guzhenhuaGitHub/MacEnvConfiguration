94a9de7ca4b04853df9e4899ca01f46b95f5f5fb
顾振华 <guzhenhuaB5@gmail.com>
2023-01-31 11:20:28 +0800

    fix

--- configure_oh_my_zsh.sh
+++ configure_oh_my_zsh.sh
@@ -3,7 +3,7 @@
 # 读取.zshrc配置
 zshrc_file=$(cat ~/.zshrc)
 # 设置oh_my_zsh插件
-zshrc_file=${zshrc_file//'plugins=(git)'/'plugins=(git cp extract aliias-finder)'}
+zshrc_file=${zshrc_file//'plugins=(git)'/'plugins=(git cp extract alias-finder)'}
 # 设置oh_my_zsh主题
 zshrc_file=${zshrc_file//'ZSH_THEME="robbyrussell"'/'ZSH_THEME="ys"'}
 # 保存设置
